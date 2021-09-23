dc6c80d42be2cf1037a9c5ee4bcd4d464776a0fa
Christian Amesöder <christian.amesoeder@stud.uni-regensburg.de>
2021-09-23 10:48:30 +0200

    create.index list updated with description

--- R/create.index.list.R
+++ R/create.index.list.R
@@ -1,12 +1,26 @@
+#' Create index list
+#'
+#' This Function is used to generate initial hmdb_indices data.frame
+#' which can be used to match CAS, InChI, InChIKey, chemspider ID, chebi ID,
+#' Pubchem ID, metlin ID, Name or SMILES to the HMDB ID using function
+#' get.id.
+#'
+#' Function expects local path to the downloaded XML file from HMDB.
+#'
+#'
+#' @param path Path to the downloaded HMDB XML database
+#' @return A data.frame with indices
+#' @export
+
 create.index.list<- function(path){
-  
+
   hmdb_data<- XML::xmlToDataFrame(path)
 
-  hmdb_indices<-c("accession", "name", "cas_registry_number", "smiles", "inchi", "inchikey", 
+  hmdb_indices<-c("accession", "name", "cas_registry_number", "smiles", "inchi", "inchikey",
                   "chemspider_id", "chebi_id", "pubchem_compound_id",
                   "metlin_id")
-    
-  
+
+
   hmdb_indices$canonical.smiles<-0
   for (i in 1:nrow(hmdb_indices)) {
     smi <- rcdk::parse.smiles(as.character(hmdb_indices$smiles[i]))[[1]]
